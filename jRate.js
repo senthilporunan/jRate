b8c98b21a758c0772e2cd9faae4aead658b3f16a
princejwesley <princejohnwesley@gmail.com>
2014-12-29 15:45:14 +0530

    :lipstick: Refactoring

--- jRate.js
+++ jRate.js
@@ -24,293 +24,263 @@ SOFTWARE.
 
 ;(function ($, undefined) {
 
-    "use strict";
-	//Default values
-	var Options = [], Node = [];
-			
-	var defaultValues = {
-		rating: "3",
-		shape: "STAR",
-		count: "5",
-		width: "40",
-		height: "50",
-		normalFill: "white",
-		startRatedFill: "green",
-		minValue: 0,
-		maxValue: 5,
-		precision: 1,
-		readOnly: false,
-		onChange: null,
-		onSet: null
-	};
-	
-	function isDefined(name) {
-		return typeof name !==  "undefined";
-	}
-	
-	/**
-	** Constructor begins here for Rating
-	**/
+    $.fn.jRate = function(options) {
 
-	function JRate(node, options) {
-	
-		//Validation implementation
-		//Mini to max size
-		//Left to Right and Right to Left  -- Done
-		//Multiple color --- done
-		//Event Handling
-		//Horizontal and  vertical support 
-		
+		"use strict";
+        var $jRate = $(this);
 
-		this.initialize = function (optionName, param) {
+		var defaults = {
+			rating: 0,
+			shape: "STAR",
+			count: "5",
+			width: "20",
+			height: "20",
+			normalColor: "white",
+            startColor: "yellow",
+            endColor: "green",
+            opacity: 1,
+			min: 0,
+			max: 5,
+			precision: 1,
+			readOnly: false,
+			onChange: null,
+			onSet: null
+		};
+        var settings = $.extend({}, defaults, options);
+        var startColorCoords, endColorCoords;
 
-			if (!isDefined(optionName) || !isDefined(param)) {
-				return options;
-			}
-			
-			//initialize node to some basic css property
-			node.css("white-space", "nowrap");
-			node.css("cursor","pointer");
-			
-			Options = options;
-			Node = node;
-			switch (optionName) {
-				case "shape":
-					setShape(node, param);
+		function isDefined(name) {
+			return typeof name !==  "undefined";
+		}
+
+		function getRating() {
+			if (isDefined(settings))
+				return settings.rating;
+		}
+
+        function setRating(rating) {
+            if(!isDefined(rating) || rating < settings.min || rating > settings.max)
+                throw rating + " is not in range(" + min + "," + max + ")";
+            showRating(rating);
+        }
+
+		function setShape() {
+			var header = '<svg width="' + settings.width + '" height=' + settings.height +' xmlns="http://www.w3.org/2000/svg" xmlns:xlink=\"http://www.w3.org/1999/xlink\"';
+			var linearGrad = '<defs><linearGradient id="grad">'+
+								'<stop offset="0%"  stop-color='+settings.normalColor+'/>'+
+								'<stop offset="0%" stop-color='+settings.normalColor+'/>'+
+							'</linearGradient></defs>';
+            var shapeRate;
+			switch(settings['shape']) {
+				case 'STAR':
+					shapeRate = header + 'viewBox="0 12.705 512 486.59"' + '>'
+							+ linearGrad
+							+ '<polygon style="fill: url(#grad);stroke:black;stroke-width:2px;" '
+							+  'points="256.814,12.705 317.205,198.566'
+										+  ' 512.631,198.566 354.529,313.435 '
+										+  '414.918,499.295 256.814,384.427 '
+										+  '98.713,499.295 159.102,313.435 '
+										+  '1,198.566 196.426,198.566 "/>'
+										+ '</svg>';
 					break;
-				case "normalFill":
-					setNormalFill(node, param);
+				case 'CIRCLE':
+					shapeRate = header + '>' + linearGrad
+							+ '<circle  cx="'+settings.width/2+'" cy="'+settings.height/2+'" r="'+settings.width/2+'" fill="url(#grad)" style="stroke:black;stroke-width:2px;"/>'
+							+ '</svg>';
+					break;
+				case 'RECTANGLE':
+					shapeRate = header + '>' + linearGrad + '<rect width="'+settings.width+'" height="'+settings.height+'" fill="url(#grad)" style="stroke:black;stroke-width:2px;"/>'+
+									'</svg>';
+					break;
+				case 'TRIANGLE':
+					shapeRate = header  + '>'
+							+ linearGrad +
+							'<polygon points="'+settings.width/2+',0 0,'+settings.height+' '+settings.width+','+settings.height+'" fill="url(#grad)" style="stroke:black;stroke-width:2px;"/>'+
+							'</svg>';
+					break;
+				case 'RHOMBUS':
+					shapeRate = header  + '>'
+						+ linearGrad
+						+ '<polygon points="'+settings.width/2+',0 '+settings.width+','+settings.height/2+' '+settings.width/2+','+settings.height+' 0,'+settings.height/2
+						+ '" fill="url(#grad)"  style="stroke:black;stroke-width:2px;"/>'
+						+ '</svg>';
 					break;
 				default:
-					break;
+					throw Error("No such shape as " + settings['shape']);
 			}
 
-			bindEvents(node);
-			return options[optionName];
-		};	
-	}
-	
-	function getRating() {
-		if (isDefined(Options))
-			return Options.rating;
-		else	
-			return 0;
-	}
-	
-	function setShape(node, paramValue) {
-		var header = '<svg width="' + Options.width + '" height=' + Options.height +' xmlns="http://www.w3.org/2000/svg" xmlns:xlink=\"http://www.w3.org/1999/xlink\"';
-		var linearGrad = '<defs><linearGradient id="grad">'+
-							'<stop offset="0%"  stop-color='+Options.normalFill+'/>'+
-							'<stop offset="0%" stop-color='+Options.normalFill+'/>'+
-						'</linearGradient></defs>';
-		switch(paramValue) {
-			case 'STAR':
-				Options.shape = header + 'viewBox="0 12.705 512 486.59"' + '>'
-						+ linearGrad 
-						+ '<polygon style="fill: url(#grad);stroke:black;stroke-width:2px;" '
-                        +  'points="256.814,12.705 317.205,198.566'
-									+  ' 512.631,198.566 354.529,313.435 '
-                                    +  '414.918,499.295 256.814,384.427 '
-                                    +  '98.713,499.295 159.102,313.435 '
-                                    +  '1,198.566 196.426,198.566 "/>'
-									+ '</svg>';
-				break;
-			case 'CIRCLE':
-				Options.shape = header + '>' + linearGrad 
-						+ '<circle  cx="'+Options.width/2+'" cy="'+Options.height/2+'" r="'+Options.width/2+'" fill="url(#grad)" style="stroke:black;stroke-width:2px;"/>'
-						+ '</svg>';
-				break;
-			case 'RECTANGLE':
-				Options.shape = header + '>' + linearGrad + '<rect width="'+Options.width+'" height="'+Options.height+'" fill="url(#grad)" style="stroke:black;stroke-width:2px;"/>'+
-								'</svg>';
-				break;
-			case 'TRIANGLE':
-				Options.shape = header  + '>' 
-						+ linearGrad + 
-						'<polygon points="'+Options.width/2+',0 0,'+Options.height+' '+Options.width+','+Options.height+'" fill="url(#grad)" style="stroke:black;stroke-width:2px;"/>'+
-						'</svg>';
-				break;
-			case 'RHOMBUS':
-				Options.shape = header  + '>' 
-					+ linearGrad 
-					+ '<polygon points="'+Options.width/2+',0 '+Options.width+','+Options.height/2+' '+Options.width/2+','+Options.height+' 0,'+Options.height/2
-					+ '" fill="url(#grad)"  style="stroke:black;stroke-width:2px;"/>'
-					+ '</svg>';
-				break;
-			default:
-				throw Error("No such shape as " + paramValue);
+            drawShape(shapeRate);
 		}
-	}
-	
-	function setNormalFill(node, paramValue) {
-		node.css('fill', paramValue);
-	}
-	
-	function bindEvents(node) {
-		node.on("mousemove", onMouseEnter)
-			.on("mouseenter", onMouseEnter)
-			.on("mouseover", onMouseEnter)
-			.on("hover", onMouseEnter)
-			.on("mouseleave", onMouseLeave)
-			.on("mouseout", onMouseLeave)
-			.on("click", onMouseClick)
-            .on("JRate.change", onChange)
-            .on("JRate.set", onSet);
-	}
-	
-	function showNormalRating(node) {
-		for (var i = 0; i < Options.count; i++) {
-			node.find("svg").eq(i).find("#grad").find("stop").eq(0).attr({'offset': '0%'});
-			node.find("svg").eq(i).find("#grad").find("stop").eq(0).attr({'stop-color': Options.normalFill});
-			node.find("svg").eq(i).find("#grad").find("stop").eq(1).attr({'offset': '0%'});
-			node.find("svg").eq(i).find("#grad").find("stop").eq(1).attr({'stop-color': Options.normalFill});
+
+		function setCSS() {
+            // setup css properies
+            $jRate.css("white-space", "nowrap");
+            $jRate.css("cursor","pointer");
+
+			$jRate.css('fill', settings['shape']);
 		}
-	}
-	
-	function showRating(rating) {
-		showNormalRating(Node);
-		var singleValue = (Options.maxValue - Options.minValue)/Options.count;
-		rating = (rating-Options.minValue)/singleValue;
-		var startFill = Options.startRatedFill;
-		
-		//Right To Left operation 
-		if (Options.rightToLeft) {
-			for (var i = 0; i < rating; i++) {
-				//console.log("Start Fill: "+startFill);
-				Node.find("svg").eq(Options.count-1-i).find("#grad").find("stop").eq(0).attr({'offset': '100%'});
-				Node.find("svg").eq(Options.count-1-i).find("#grad").find("stop").eq(0).attr({'stop-color': startFill});
-				if (rating*10 % 10 > 0) {
-					Node.find("svg").eq(Math.ceil(Options.count-rating)-1).find("#grad").find("stop").eq(0).attr({'offset': 100-(rating*10 %10)*10+'%'});
-					Node.find("svg").eq(Math.ceil(Options.count-rating)-1).find("#grad").find("stop").eq(0).attr({'stop-color': Options.normalFill});
-					Node.find("svg").eq(Math.ceil(Options.count-rating)-1).find("#grad").find("stop").eq(1).attr({'offset': 100-(rating*10 %10)*10+'%'});
-					Node.find("svg").eq(Math.ceil(Options.count-rating)-1).find("#grad").find("stop").eq(1).attr({'stop-color': startFill});
-				}
-				if (isDefined(Options.endRatedFill)) {
-					var startRatedFill = colorToRGBA(Options.startRatedFill);
-					var endRatedFill = colorToRGBA(Options.endRatedFill);
-					startFill = formulateNewColor(Options.count-1, i, startRatedFill, endRatedFill);
+
+		function bindEvents() {
+			$jRate.on("mousemove", onMouseEnter)
+				.on("mouseenter", onMouseEnter)
+				.on("mouseover", onMouseEnter)
+				.on("hover", onMouseEnter)
+				.on("mouseleave", onMouseLeave)
+				.on("mouseout", onMouseLeave)
+				.on("click", onMouseClick)
+				.on("JRate.change", onChange)
+				.on("JRate.set", onSet);
+		}
+
+		function showNormalRating() {
+			for (var i = 0; i < settings.count; i++) {
+				$jRate.find("svg").eq(i).find("#grad").find("stop").eq(0).attr({'offset': '0%'});
+				$jRate.find("svg").eq(i).find("#grad").find("stop").eq(0).attr({'stop-color': settings.normalColor});
+				$jRate.find("svg").eq(i).find("#grad").find("stop").eq(1).attr({'offset': '0%'});
+				$jRate.find("svg").eq(i).find("#grad").find("stop").eq(1).attr({'stop-color': settings.normalColor});
+			}
+		}
+
+		function showRating(rating) {
+
+			showNormalRating();
+			var singleValue = (settings.max - settings.min)/settings.count;
+			rating = (rating-settings.min)/singleValue;
+			var fillColor = settings.startColor;
+
+			//Right To Left operation
+			if (settings.rightToLeft) {
+				for (var i = 0; i < rating; i++) {
+					$jRate.find("svg").eq(settings.count-1-i).find("#grad").find("stop").eq(0).attr({'offset': '100%'});
+					$jRate.find("svg").eq(settings.count-1-i).find("#grad").find("stop").eq(0).attr({'stop-color': fillColor});
+					if (rating*10 % 10 > 0) {
+                        console.log('inside if ' + rating);
+						$jRate.find("svg").eq(Math.ceil(settings.count-rating)-1).find("#grad").find("stop").eq(0).attr({'offset': 100-(rating*10 %10)*10+'%'});
+						$jRate.find("svg").eq(Math.ceil(settings.count-rating)-1).find("#grad").find("stop").eq(0).attr({'stop-color': settings.normalColor});
+						$jRate.find("svg").eq(Math.ceil(settings.count-rating)-1).find("#grad").find("stop").eq(1).attr({'offset': 100-(rating*10 %10)*10+'%'});
+						$jRate.find("svg").eq(Math.ceil(settings.count-rating)-1).find("#grad").find("stop").eq(1).attr({'stop-color': fillColor});
+					} else {
+                        console.log('inside else');
+                        console.log(rating);
+                    }
+					if (isDefined(endColorCoords)) {
+						fillColor = formulateNewColor(settings.count-1, i, startColorCoords, endColorCoords);
+					}
 				}
 			}
-			return;
+            else {
+			    for (var i=0; i < rating; i++) {
+                    $jRate.find("svg").eq(i).find("#grad").find("stop").eq(0).attr({'offset': '100%'});
+				    $jRate.find("svg").eq(i).find("#grad").find("stop").eq(0).attr({'stop-color': fillColor});
+                    if (rating*10 % 10 > 0) {
+                        $jRate.find("svg").eq(Math.ceil(rating)-1).find("#grad").find("stop").eq(0).attr({'offset': (rating*10 %10)*10+'%'});
+                        $jRate.find("svg").eq(Math.ceil(rating)-1).find("#grad").find("stop").eq(0).attr({'stop-color': fillColor});
+				    }
+                    if (isDefined(endColorCoords)) {
+                        fillColor = formulateNewColor(settings.count, i, startColorCoords, endColorCoords);
+				    }
+			    }
+            }
+            // set current rating
+            settings.rating = rating;
 		}
-		
-		for (var i=0; i < rating; i++) {
-			Node.find("svg").eq(i).find("#grad").find("stop").eq(0).attr({'offset': '100%'});
-			Node.find("svg").eq(i).find("#grad").find("stop").eq(0).attr({'stop-color': startFill});
-			if (rating*10 % 10 > 0) {
-				Node.find("svg").eq(Math.ceil(rating)-1).find("#grad").find("stop").eq(0).attr({'offset': (rating*10 %10)*10+'%'});
-				Node.find("svg").eq(Math.ceil(rating)-1).find("#grad").find("stop").eq(0).attr({'stop-color': startFill});
+
+		var formulateNewColor = function(totalCount, currentVal, startFill, endFill) {
+			var avgFill = [];
+			for (var i=0; i < 3; i++) {
+				var diff = Math.round((startFill[i] - endFill[i])/totalCount);
+				var newValue = startFill[i] + (diff * (currentVal+1));
+				if (newValue/256)
+					avgFill[i] = (startFill[i] - (diff * (currentVal+1)))%256;
+				else
+					avgFill[i] = (startFill[i] + (diff * (currentVal+1)))%256;
 			}
-			if (isDefined(Options.endRatedFill)) {
-				var startRatedFill = colorToRGBA(Options.startRatedFill);
-				var endRatedFill = colorToRGBA(Options.endRatedFill);
-				startFill = formulateNewColor(Options.count, i, startRatedFill, endRatedFill);
+			return "rgba("+avgFill[0]+","+avgFill[1]+","+avgFill[2]+"," + settings.opacity + ")";
+		};
+
+
+
+		function colorToRGBA(color) {
+			var cvs, ctx;
+			cvs = document.createElement('canvas');
+			cvs.height = 1;
+			cvs.width = 1;
+			ctx = cvs.getContext('2d');
+			ctx.fillStyle = color;
+			ctx.fillRect(0, 0, 1, 1);
+			return ctx.getImageData(0, 0, 1, 1).data;
+		}
+
+		function onMouseEnter(e) {
+			if (settings.readOnly) return;
+			var pageX = settings.rightToLeft ? (settings.count*settings.width)-e.pageX : e.pageX;
+			var pageLeft = $jRate.offset().left;
+
+			var singleValue = (settings.max - settings.min)/settings.count;
+			var rating = (((pageX-pageLeft)/settings.width)) * singleValue;
+			rating = settings.min + Number(rating.toFixed(settings.precision));
+			showRating(rating);
+			$jRate.trigger("JRate.change", {rating: rating});
+		}
+
+		function onMouseLeave() {
+			if (!settings.readOnly) {
+				showRating(settings.rating);
 			}
 		}
-	}
-	
-	var formulateNewColor = function(totalCount, currentVal, startFill, endFill) {
-		var avgFill = [];
-		for (var i=0; i < 3; i++) {
-			//console.log("StartFill: "+startFill[i]+" End Fill: "+endFill[i]);
-			var diff = Math.round((startFill[i] - endFill[i])/totalCount);
-			var newValue = startFill[i] + (diff * (currentVal+1));
-			if (newValue/256)
-				avgFill[i] = (startFill[i] - (diff * (currentVal+1)))%256;
-			else	
-				avgFill[i] = (startFill[i] - (diff * (currentVal+1)))%256;
-			//console.log("i = "+i+" Value: "+avgFill[i]+" Diff: "+diff);
-		}
-		//console.log("rgb("+avgFill[0]+","+avgFill[1]+","+avgFill[2]+",1)");
-		return "rgba("+avgFill[0]+","+avgFill[1]+","+avgFill[2]+",1)";
-	};
-	
 
+		function onMouseClick(e) {
+			if (settings.readOnly) return;
 
-	function colorToRGBA(color) {
-		var cvs, ctx;
-		cvs = document.createElement('canvas');
-		cvs.height = 1;
-		cvs.width = 1;
-		ctx = cvs.getContext('2d');
-		ctx.fillStyle = color;
-		ctx.fillRect(0, 0, 1, 1);
-		return ctx.getImageData(0, 0, 1, 1).data;
-	}
-	
-	function onMouseEnter(e) {
-		var pageX = Options.rightToLeft ? (Options.count*Options.width)-e.pageX : e.pageX;
-		var pageLeft = Node.offset().left;
-		
-		var singleValue = (Options.maxValue - Options.minValue)/Options.count;
-		var rating = (((pageX-pageLeft)/Options.width)) * singleValue;
-		rating = Options.minValue + Number(rating.toFixed(Options.precision));
-		if (!Options.readOnly) {
-			showRating(rating); 
-		}
-		Node.trigger("JRate.change", {rating: rating});
-	}
-	
-	function onMouseLeave() {
-		if (!Options.readOnly) {
-			showRating(Options.rating);
-		}
-	}
-	
-	function onMouseClick(e) {
-		var pageX = Options.rightToLeft ? (Options.count*Options.width)-e.pageX : e.pageX;
-		var pageLeft = Node.offset().left;
-		
-		var singleValue = (Options.maxValue - Options.minValue)/Options.count;
-		var rating = (((pageX-pageLeft)/Options.width)) * singleValue;
-		if (!Options.readOnly) {
-			Options.rating = Options.minValue + Number(rating.toFixed(Options.precision));
-			showRating(Options.rating);
-		}
-		//console.log("Rating: "+Options.rating);
-		Node.trigger("JRate.set", {rating: Options.rating});
-	}
-	
-	function onChange(e, data) {
-		if(Options.onChange && typeof Options.onChange === "function") {
-			Options.onChange.apply(this, [data.rating]);
-      }
-	}
-	
-	function onSet(e, data) {
-		if(Options.onSet && typeof Options.onSet === "function") {
-			Options.onSet.apply(this, [data.rating]);
-      }
-	}
-	function drawShape(node) {
-		for (var i = 0; i < Options.count; i++) {
-			node.append(Options.shape);
-		}
-		
-		showNormalRating(node);
-		showRating(Options.rating);
-		node.find("svg")
-                  .attr({width: Options.width,height: Options.height});
-	}
-	
-	
-	function _jRate(options) {
-		
-		var node = $(this);
-		options = $.extend(JSON.parse(JSON.stringify(defaultValues)), options);
-		var newObject = new JRate(node, options);
-		
-		$.each(options, function (paramName, paramValue) {
-			newObject.initialize(paramName, paramValue);
-		});
-		drawShape(node);
-	}
-		
-	function jRate() {
-		return _jRate.apply(this, Array.prototype.slice.apply(arguments, []));
-	}
-		
-	$.fn.jRate = jRate;
+			var pageX = settings.rightToLeft ? (settings.count*settings.width)-e.pageX : e.pageX;
+			var pageLeft = $jRate.offset().left;
 
+			var singleValue = (settings.max - settings.min)/settings.count;
+			var rating = (((pageX-pageLeft)/settings.width)) * singleValue;
+			settings.rating = settings.min + Number(rating.toFixed(settings.precision));
+			showRating(settings.rating);
+			$jRate.trigger("JRate.set", {rating: settings.rating});
+		}
+
+		function onChange(e, data) {
+			if(settings.onChange && typeof settings.onChange === "function") {
+				settings.onChange.apply(this, [data.rating]);
+			}
+		}
+
+		function onSet(e, data) {
+			if(settings.onSet && typeof settings.onSet === "function") {
+				settings.onSet.apply(this, [data.rating]);
+			}
+		}
+		function drawShape(shapeRate) {
+			for (var i = 0; i < settings.count; i++) {
+				$jRate.append(shapeRate);
+			}
+
+			showNormalRating();
+			showRating(settings.rating);
+			$jRate.find("svg").attr({width: settings.width,height: settings.height});
+		}
+
+        //TODO
+        //Validation implementation
+        //Mini to max size
+        //Horizontal and  vertical support
+
+        //TODO Add this as a part of validation
+        if (settings.startColor) startColorCoords = colorToRGBA(settings.startColor);
+        if (settings.endColor) endColorCoords = colorToRGBA(settings.endColor);
+
+        setCSS();
+        setShape();
+        bindEvents();
+
+        return $.extend({}, this, {
+            "getRating": getRating,
+            "setRating": setRating,
+            "setReadOnly": function(flag) { settings.readOnly = flag; },
+            "isReadOnly": function() { return settings.readOnly },
+        });
+    };
 }(jQuery));
-
