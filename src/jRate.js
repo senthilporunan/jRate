49ce63c2354ea86837dff9c953d2ed1d4ca35b4a
Senthil Porunan <senthilraja39@gmail.com>
2015-01-09 23:44:14 +0530

    Added git link in comment

--- src/jRate.js
+++ src/jRate.js
@@ -20,6 +20,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
+Source: https://github.com/senthilporunan/jRate
+Demo: http://www.toolitup.com/JRate.html
 **/
 ;
 (function($, undefined) {
