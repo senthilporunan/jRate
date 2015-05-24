eab297c58eb31b7bec52e710b3254dc6bede3cd9
unknown <sporunan@L-BLR-00800624.corp.ebay.com>
2015-05-24 13:56:53 +0530

    Fix :bug: #15 set rating after reset

--- src/jRate.js
+++ src/jRate.js
@@ -71,6 +71,7 @@ Demo: http://www.toolitup.com/JRate.html
         function setRating(rating) {
             if (!isDefined(rating) || rating < settings.min || rating > settings.max)
                 throw rating + " is not in range(" + min + "," + max + ")";
+			settings.rating = rating;
             showRating(rating);
         }
 
